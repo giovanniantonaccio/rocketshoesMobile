231963b5541c332a1b5f09a9bd48ce428b047dc9
Giovanni Antonaccio <giovanniantonaccio@gmail.com>
2019-08-20 00:44:19 -0400

    fix: axios base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3333'
-      : 'https://my-json-server.typicode.com/giovanniantonaccio/rocketshoesMobile',
+  baseURL: __DEV__
+    ? 'http://localhost:3333'
+    : 'https://my-json-server.typicode.com/giovanniantonaccio/rocketshoesMobile',
 });
 
 export default api;
